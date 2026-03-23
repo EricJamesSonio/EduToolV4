9591e303ddf869160be59ab4d438b869d1d0e8e4
EricSonio <ericjamessonio7@gmail.com>
2026-03-23 17:50:54 +0800

    Updated the main

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+import { ValidationPipe } from './commons/pipes/validation.pipe';
+import { HttpExceptionFilter } from './commons/filters/http-exception.filter';
+import { AllExceptionFilter } from './commons/filters/all-exception.filter';
+import { LoggingInterceptor } from './commons/interceptors/logging.interceptor';
+import { ResponseInterceptor } from './commons/interceptors/response.interceptor';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  app.useGlobalFilters(
+    new HttpExceptionFilter(),
+    new AllExceptionFilter(),
+  );
+
+  app.useGlobalInterceptors(
+    new LoggingInterceptor(),
+    new ResponseInterceptor(),
+  );
+
+  await app.listen(3000);
 }
 bootstrap();
