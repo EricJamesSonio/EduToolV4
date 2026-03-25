74c83b44b3af4a2170e38a5fe356df4a0bcdd1aa
EricSonio <ericjamessonio7@gmail.com>
2026-03-25 11:13:55 +0800

    Updated the api endpoints

--- backend/src/modules/assessment/assessment.module.ts
+++ backend/src/modules/assessment/assessment.module.ts
@@ -1,9 +1,17 @@
+// src/modules/assessment/assessment.module.ts
 import { Module } from '@nestjs/common';
-import { AssessmentService } from './assessment.service';
 import { AssessmentController } from './assessment.controller';
+import { AssessmentService } from './assessment.service';
+import { AssessmentRepository } from './assessment.repository';
+import { LessonModule } from '../lesson/lesson.module';
+import { ClassModule } from '../class/class.module';
+import { AuditLogModule } from '../audit-log/audit-log.module';
+import { NotificationModule } from '../notification/notification.module';
 
 @Module({
-  providers: [AssessmentService],
-  controllers: [AssessmentController]
+  imports: [LessonModule, ClassModule, AuditLogModule, NotificationModule],
+  controllers: [AssessmentController],
+  providers: [AssessmentService, AssessmentRepository],
+  exports: [AssessmentService, AssessmentRepository],
 })
 export class AssessmentModule {}
