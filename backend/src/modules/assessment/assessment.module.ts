3b5d1b48aabbd39d01e39c66d525a53085537e31
EricSonio <ericjamessonio7@gmail.com>
2026-03-25 16:07:09 +0800

    Updated the module

--- backend/src/modules/assessment/assessment.module.ts
+++ backend/src/modules/assessment/assessment.module.ts
@@ -1,21 +1,10 @@
 // @/modules/assessment/assessment.module.ts
 import { Module } from '@nestjs/common';
-import { AssessmentController } from './assessment.controller';
-import { AssessmentService } from './assessment.service';
-import { AssessmentRepository } from './assessment.repository';
-import { LessonModule } from '../lesson/lesson.module';
-import { ClassModule } from '../class/class.module';
-import { AuditLogModule } from '../audit-log/audit-log.module';
-import { NotificationModule } from '../notification/notification.module';
-import { AttendanceModule } from '../attendance/attendance.module';
-import { AssessmentStudentController } from './assessment.student.controller';
-import { AssessmentStudentService } from './assessment.student.service';
-import { AssessmentCoreService } from './assessment.core.service';
+import { AssessmentEducatorModule } from './educator/assessment-educator.module';
+import { AssessmentStudentModule } from './student/assessment-student.module';
 
 @Module({
-  imports: [LessonModule, ClassModule, AuditLogModule, NotificationModule, AttendanceModule],
-  controllers: [AssessmentController, AssessmentStudentController],
-  providers: [AssessmentService, AssessmentRepository, AssessmentStudentService, AssessmentCoreService],
-  exports: [AssessmentService, AssessmentRepository],
+  imports: [AssessmentEducatorModule, AssessmentStudentModule],
+  exports: [AssessmentEducatorModule],
 })
 export class AssessmentModule {}
