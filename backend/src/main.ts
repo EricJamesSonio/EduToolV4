d8affb575080389f30cad4f8a4d8b9da9ff4f3d5
EricSonio <ericjamessonio7@gmail.com>
2026-03-26 12:14:08 +0800

    Updated the main to have io adapater

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,7 +1,8 @@
-import 'dotenv/config'; // ← must be first, before anything else
+import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { IoAdapter } from '@nestjs/platform-socket.io';          // 👈 add
 import { HttpExceptionFilter } from './commons/filters/http-exception.filter';
 import { AllExceptionFilter } from './commons/filters/all-exception.filter';
 import { LoggingInterceptor } from './commons/interceptors/logging.interceptor';
@@ -10,13 +11,13 @@ import { ResponseInterceptor } from './commons/interceptors/response.interceptor
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes(new ValidationPipe());
+  app.useWebSocketAdapter(new IoAdapter(app));                   // 👈 add
 
+  app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(
     new HttpExceptionFilter(),
     new AllExceptionFilter(),
   );
-
   app.useGlobalInterceptors(
     new LoggingInterceptor(),
     new ResponseInterceptor(),
