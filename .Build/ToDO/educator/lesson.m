d86a78a7bce2b45aaceea77768ec74f734914e8c
EricSonio <ericjamessonio7@gmail.com>
2026-04-17 10:52:24 +0800

    Todo

---
+++ .Build/ToDO/educator/lesson.m
@@ -0,0 +1,17 @@
+The class weeks are correctly using the semester templates but its not doing the coerrect approach.
+
+currently for example the semester setting has first sem and second sem, and for example it has 19 weeks in total.
+
+so the gap between first and second sem is not coutned there. so its correct.
+
+so the week count is week 1 - 19
+
+now the problem is the counting should reset to 1, when the second sem is there.. so will be for example first sem just have 9 weeks in total, then 10 weeks in second sem,.. so the count will be
+
+first sem :
+1-9 weeks
+
+second sem :
+1-10 weeks 
+
+so bascaily it resets since this is instructional weeks.nomt calendar week
