3c75ac6152aa7edf665cbf4fca8c3fd5b156e612
EricSonio <ericjamessonio7@gmail.com>
2026-08-08 19:09:41 +0800

    Update user-domain.module

--- backend/src/domains/user/user-domain.module.ts
+++ backend/src/domains/user/user-domain.module.ts
@@ -3,9 +3,10 @@ import { Module } from '@nestjs/common';
 
 import { StudentModule } from '@/modules/student/student.module';
 import { EducatorModule } from '@/modules/educator/educator.module';
+import { RegistrarModule } from '@/modules/registrar/registrar.module';
 
 @Module({
-  imports: [StudentModule, EducatorModule],
-  exports: [StudentModule, EducatorModule],
+  imports: [StudentModule, EducatorModule, RegistrarModule],
+  exports: [StudentModule, EducatorModule, RegistrarModule],
 })
 export class UserDomainModule {}
