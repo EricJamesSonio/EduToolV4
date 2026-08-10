6f5b9a64950ea53c08208d949c4bfe750c13d573
EricSonio <ericjamessonio7@gmail.com>
2026-08-10 22:49:52 +0800

    Add concern.module

---
+++ backend/src/modules/concern/concern.module.ts
@@ -0,0 +1,17 @@
+// src/modules/concern/concern.module.ts
+import { Module } from '@nestjs/common';
+import { ConcernCoreModule } from './core/concern-core.module';
+import { ConcernStudentModule } from './student/concern-student.module';
+import { ConcernStaffModule } from './staff/concern-staff.module';
+import { ConcernCategoryModule } from './category/concern-category.module';
+
+@Module({
+  imports: [
+    ConcernCoreModule,
+    ConcernStudentModule,
+    ConcernStaffModule,
+    ConcernCategoryModule,
+  ],
+  exports: [ConcernCoreModule, ConcernStudentModule, ConcernStaffModule],
+})
+export class ConcernModule {}
