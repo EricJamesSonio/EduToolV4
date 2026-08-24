ffb0fb1c4c835066f0ab0d1f71a1166a6066d04f
EricSonio <ericjamessonio7@gmail.com>
2026-08-24 23:26:44 +0800

    Update tailwind.config

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -98,6 +98,11 @@ colors: {
     "3": "var(--chart-3)",
     "4": "var(--chart-4)",
     "5": "var(--chart-5)",
+    "6": "var(--chart-6)",
+    "7": "var(--chart-7)",
+    "8": "var(--chart-8)",
+    "9": "var(--chart-9)",
+    "10": "var(--chart-10)",
   },
 },
       borderRadius: {
